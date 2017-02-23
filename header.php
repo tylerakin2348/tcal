f1c85c08027371618c6dec3542a308f06f71195f
Tyler Akin <takin963@students.sbts.edu>
2017-02-22 23:17:37 -0500

    setting up new branch

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Tyler Akin Web Design</title>
+  <title>The Church at Louisville</title>
   <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,300,700" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Crimson+Text:400,400italic,600,700" rel="stylesheet" type="text/css">
 <?php wp_head(); ?>

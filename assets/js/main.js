18e62a0bcedb02cccc2c4cdb0ac52e1456b92e3a
Tyler Akin <takin963@students.sbts.edu>
2017-02-24 22:49:20 -0500

    removes unneeeded JQuery from project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,6 @@
-// This code contains the hide/show functionality of the design options
-
-$(".design-info-word").hide();
-$("#wordpress-block").click(function() {
-  if ($(".design-info-word").is(":hidden")) {
-    $(".design-info-scratch").hide();
-    $(".design-info-maintain").hide();
-    $(".design-info-word").slideDown("slow");
-  } else {
-    $(".design-info-word").slideUp("fast");
-  }
-});
-
-
-$(".design-info-scratch").hide();
-$("#custom-work-block").click(function() {
-  if ($(".design-info-scratch").is(":hidden")) {
-    $(".design-info-word").hide();
-    $(".design-info-maintain").hide();
-    $(".design-info-scratch").slideDown("slow");
-  } else {
-    $(".design-info-scratch").slideUp("fast");
-  }
-});
-
-$(".design-info-maintain").hide();
-$("#template-block").click(function() {
-  if ($(".design-info-maintain").is(":hidden")) {
-    $(".design-info-word").hide();
-    $(".design-info-scratch").hide();
-    $(".design-info-maintain").slideDown("slow");
-  } else {
-    $(".design-info-maintain").slideUp("fast");
-  }
-});
-
-$(".design-option-link").click(function() {
-  return false;
-});
-
 // This controls show-nav functionality
 
 // $(".main-navigation").hide();
-
 
 $("#show-nav").click(function() {
   if ($(".menu-menu-1-container").is(":hidden")) {
@@ -62,42 +21,3 @@ $('#show-nav').click(function() {
     $(this).text("Show Menu");
   }
 });
-
-// This hides the design option info that slides up in blue.
-
-//Template info
-$(".hide-info-button-template").click(function() {
-  if ($(".design-info-maintain").is(":hidden")) {
-    $(".design-info-maintain").show();
-  } else {
-    $(".design-info-maintain").hide();
-  }
-});
-// Wordpress info
-$(".hide-info-button-word").click(function() {
-  if ($(".design-info-word").is(":hidden")) {
-    $(".design-info-word").show();
-  } else {
-    $(".design-info-word").hide();
-  }
-});
-//Scratch info
-$(".hide-info-button-scratch").click(function() {
-  // return false;
-  if ($(".design-info-scratch").is(":hidden")) {
-    $(".design-info-scratch").show();
-  } else {
-    $(".design-info-scratch").hide();
-  }
-});
-
-
-
-// Greensocks Button Animation
-
-
-// TweenLite.to(div, 1, {x:100});
-//
-//   $(".template-hidden").hide(); // text of blue section
-//   $(".design-info-maintain").hide(); // blue overlay
-// });
